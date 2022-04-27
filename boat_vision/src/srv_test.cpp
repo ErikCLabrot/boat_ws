b4a276c1e438656f64aacdeefdf01419393a215c
ErikCLabrot <ecb25@students.uwf.edu>
2022-04-27 11:35:48 -0500

    Undid some debugging

--- boat_vision/src/srv_test.cpp
+++ boat_vision/src/srv_test.cpp
@@ -41,81 +41,8 @@ int main(int argc, char* argv[])
     ros::Subscriber compass_sub = nh.subscribe(compass_topic, 1000, compassCB);
     ros::ServiceClient wpclnt = nh.serviceClient<mavros_msgs::WaypointPush>("mavros/mission/push");
     ros::ServiceClient distclnt = nh.serviceClient<boat_vision::calc_gps>("calc_gps");
-    
-            std::cout << "Fix and heading acquired\n";
-            loop = false;
-            boat_vision::calc_gps wpsrvc;
-            wpsrvc.request.pos.latitude = 30.5475;
-            wpsrvc.request.pos.longitude = -87.2169;
 
-
-            mavros_msgs::Waypoint point;
-            point.frame = 0;
-            point.command = 16;
-            point.param1 = 0;
-            point.param2 = 1;
-            point.param3 = 0;
-
-            mavros_msgs::WaypointPush mission;
-
-            //wp1
-            wpsrvc.request.dist = 1;
-            wpsrvc.request.head = 68;
-            if(distclnt.call(wpsrvc))
-            {
-                std::cout << "Calculating wp1\n";
-                point.x_lat = wpsrvc.response.coord.latitude;
-                point.y_long = wpsrvc.response.coord.longitude;
-                std::cout << point.x_lat << " " << point.y_long << std::endl;
-                mission.request.waypoints.push_back(point);
-            }
-            else
-                std::cout << "distance service call failed!\n";
-
-            //wp2
-            wpsrvc.request.dist = 1.41421356237;
-            if(heading < 45)
-                wpsrvc.request.head = 360-45;
-            else
-                wpsrvc.request.head = (heading - 45);
-            if(distclnt.call(wpsrvc))
-            {
-                std::cout << "Calculating wp2\n";
-                point.x_lat = wpsrvc.response.coord.latitude;
-                point.y_long = wpsrvc.response.coord.longitude;
-                std::cout << point.x_lat << " " << point.y_long << std::endl;
-                mission.request.waypoints.push_back(point);
-            }
-            else
-                std::cout << "distance service call failed!\n";
-
-            wpsrvc.request.dist = 1;
-            if(heading < 90)
-                wpsrvc.request.head = 360-90;
-            else
-                wpsrvc.request.head = (heading - 90);
-            if(distclnt.call(wpsrvc))
-            {
-                std::cout << "Calculating wp3\n";
-                point.x_lat = wpsrvc.response.coord.latitude;
-                point.y_long = wpsrvc.response.coord.longitude;
-                std::cout << point.x_lat << " " << point.y_long << std::endl;             
-                mission.request.waypoints.push_back(point);
-            }
-            else
-                std::cout << "distance service call failed!\n";
-
-            std::cout << "Calculating wp4\n";
-            point.x_lat = gpsmsg.latitude;
-            point.y_long = gpsmsg.longitude;
-            std::cout << point.x_lat << " " << point.y_long << std::endl;
-            mission.request.waypoints.push_back(point);
-            
-            if(wpclnt.call(mission))
-                std::cout << "Mission pushed succesfully!\n";
-            else
-                std::cout << "Mission push failed!\n";
- /*   while(loop)
+    while(loop)
     {
     	std::cout << "Waiting for fix and heading\n";	    
     	if(gps_ready && reading_ready)
@@ -197,6 +124,5 @@ int main(int argc, char* argv[])
     	ros::spinOnce();
         checkRate.sleep();
     }
-*/
     return 0;
 }
